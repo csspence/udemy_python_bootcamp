a11eb065c2b44b47ff6de43cff4ac343117c1a41
Crew Spence <crewspence@gCrewSpence.local>
2021-07-15 11:30:28 -0500

    Created new file for identifiers.

---
+++ identifiers.py
@@ -0,0 +1,9 @@
+# You cannot start a variable with a number
+# You cannot use keywords as a variable name (just like in JS)
+# Variable names are case-sensitive
+
+sport = 'Football'
+Sport = 'Tennis'
+
+#get output
+print(sport, ' and ', Sport)
