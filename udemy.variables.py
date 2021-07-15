4c6704dbc6078642e98654e7bd9383df31bfa942
Crew Spence <csspence123@gmail.com>
2021-07-15 11:21:57 -0500

    Concatenated strings and variables.

--- udemy.variables.py
+++ udemy.variables.py
@@ -1,15 +1,11 @@
 # Variable declaration and assignment
-
 # name = "Ronaldo"
 # num = 7
 # age = 34
 
 # Multi-assignment within a single statement
-
 name, num, age = "Ronaldo", 7, 34
 
 # get the output
-
-print(name)
-print(num)
-print(age)
+print(name, num, age)
+print("The player name is", name, ", his age is", age, "and his number is", num)
