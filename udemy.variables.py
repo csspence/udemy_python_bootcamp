3ed0187b8b849a9952ca1bec84d49f1dd1aeec40
Crew Spence <csspence123@gmail.com>
2021-07-15 10:46:27 -0500

    Update udemy.variables.py

--- udemy.variables.py
+++ udemy.variables.py
@@ -1,12 +1,15 @@
 # Variable declaration and assignment
+
 # name = "Ronaldo"
 # num = 7
 # age = 34
 
 # Multi-assignment within a single statement
+
 name, num, age = "Ronaldo", 7, 34
 
 # get the output
+
 print(name)
 print(num)
 print(age)
