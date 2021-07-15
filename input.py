0a34cae6aeb05594beef0bdea9f3d234d4a5a9d3
Crew Spence <crewspence@gCrewSpence.local>
2021-07-15 11:37:07 -0500

    Used len, max, and min functions.

--- input.py
+++ input.py
@@ -1,6 +1,9 @@
 #Collect and accept data
 
-get_name = input('What is your name?')
+#get_name = input('What is your name?')
 
 # get output
-print('Your name is: ' + get_name)
+#print('Your name is: ' + get_name)
+#input(len('Python'))
+#input(max([423, 12, 9]))
+input(min([939, 2, 1, 0]))
