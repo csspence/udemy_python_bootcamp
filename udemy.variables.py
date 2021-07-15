0f8a3ca53656998455aaf3e686848c5da7f913cb
Crew Spence <crewspence@gCrewSpence.local>
2021-07-15 10:28:01 -0500

    Learning variables

---
+++ udemy.variables.py
@@ -0,0 +1,12 @@
+# Variable declaration and assignment
+name = "Ronaldo"
+num = 7
+age = 34
+
+# Multi-assignment within a single statement
+# name, num, age = "Ronaldo", 7, 34
+
+# get the output
+print(name)
+print(num)
+print(age)
