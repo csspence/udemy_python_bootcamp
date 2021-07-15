a958da7da5cb20b5931687ed1f9d1962081733f8
Crew Spence <crewspence@gCrewSpence.local>
2021-07-15 10:45:30 -0500

    tested mutliple variables in one line

--- udemy.variables.py
+++ udemy.variables.py
@@ -1,10 +1,10 @@
 # Variable declaration and assignment
-name = "Ronaldo"
-num = 7
-age = 34
+# name = "Ronaldo"
+# num = 7
+# age = 34
 
 # Multi-assignment within a single statement
-# name, num, age = "Ronaldo", 7, 34
+name, num, age = "Ronaldo", 7, 34
 
 # get the output
 print(name)
